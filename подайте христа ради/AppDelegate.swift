55e5e2f36e8399183859a1a0ee17937f854bebfc
Гость <Guest@iMac-Mac005-36.local>
2022-06-03 14:48:03 +0900

    d

---
+++ подайте христа ради/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  подайте христа ради
+//
+//  Created by Гость on 12.04.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+}
+
---
+++ подайте христа ради/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  подайте христа ради
+//
+//  Created by Гость on 12.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
